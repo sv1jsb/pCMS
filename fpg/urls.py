fe535bcc2284d5f59fbd4fa7eb3f9c0ee78a6f9c
sv1jsb <sv1jsb@gmail.com>
2012-09-01 00:41:15 +0300

    Changing to Django1.4

--- fpg/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from fpg.extra_views import search, elfinder_mce, elfinder_connector_mce
-urlpatterns = patterns('',
-    url(r'^search/$',search,{},name='search'),
-    url(r'^elfinder_mce/$',elfinder_mce,{},name='elfinder_mce'),
-    url(r'^elfinder_connector_mce/$',elfinder_connector_mce,{},name='elfinder_connector_mce'),
-)
--- pcomments/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from pcomments.forms import pCMSCommentForm
-
-def get_form():
-    return pCMSCommentForm
