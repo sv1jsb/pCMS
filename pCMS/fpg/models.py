eab9145fc040555b32d82a4c4cb2b7da005c01b0
sv1jsb <sv1jsb@gmail.com>
2012-09-03 21:26:37 +0300

    Updated jquery.ui css. Changed models definition

--- pCMS/fpg/models.py
+++ pCMS/fpg/models.py
@@ -3,17 +3,17 @@ from django.db import models
 from django.contrib.flatpages.models import FlatPage
 
 class Flatpage(FlatPage):
-    published = models.BooleanField("Published", default=True, blank=True)
-    scripts = models.TextField(verbose_name=u"Scripts", blank=True)
-    styles = models.TextField(verbose_name=u"Styles", blank=True)
-    keywords = models.TextField(verbose_name=u"Keywords", blank=True)
-    description = models.TextField(verbose_name=u"Description", blank=True)
+    published = models.BooleanField(default=True, blank=True)
+    scripts = models.TextField(blank=True)
+    styles = models.TextField(blank=True)
+    keywords = models.TextField(blank=True)
+    description = models.TextField(blank=True)
 
 class Menu(models.Model):
-    title = models.CharField(verbose_name=u"Title",max_length=50)
-    url = models.CharField(verbose_name=u"URL",max_length=50)
-    index = models.IntegerField(verbose_name=u"Order")
-    registration_required = models.BooleanField(verbose_name=u"Registration required")
+    title = models.CharField(max_length=50)
+    url = models.CharField(u"URL",max_length=50)
+    index = models.IntegerField(u"Order")
+    registration_required = models.BooleanField()
     
     class Meta:
         ordering = ('index',)
